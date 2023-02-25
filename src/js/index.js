4f4ebd0fb62d98ec575829429da747330488fea3
Pablo Vicente <pablovicente19@hotmail.com>
2023-02-25 14:09:08 -0300

    modicando estilos

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,8 @@ const $card = document.createElement('section')
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 const getPokemon = async (id) => {
-    const responde = await fetch(url + id)
-    const data = await responde.json() 
+    const response = await fetch(url + id)
+    const data = await response.json() 
     return data;
 };
 const createCardPokemon = (pokemon) => {
